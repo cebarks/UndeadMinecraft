e95dd690d2ac0dc956f7eaacfa1a1f578e02b4f1
Anten <cebarks@gmail.com>
2013-04-12 11:46:25 -0600

    no message

--- common/net/undead/Reference.java
+++ common/net/undead/Reference.java
@@ -4,13 +4,17 @@ import cpw.mods.fml.client.FMLClientHandler;
 import net.minecraft.client.Minecraft;
 
 public class Reference {
-	public static final String TextureLocation = "/mods/UndeadMinecraft/textures";
-	public static final String EntityTextureLocation = "/mods/UndeadMinecraft/textures/entity/";
 	public static final String MOD_ID = "UndeadMinecraft";
 	public static final String MOD_NAME = "Undead Minecraft";
+	
 	private static final int versionMajor = 2;
 	private static final int versionMinor = 0;
 	private static final int versionSubminor = 0;
+	
 	public static final String VERSION = versionMajor+"."+versionMinor+"."+versionSubminor;
+	
+	public static final String TextureLocation = "/mods/UndeadMinecraft/textures";
+	public static final String EntityTextureLocation = "/mods/UndeadMinecraft/textures/entity/";
+
 	public static final Minecraft MC = FMLClientHandler.instance().getClient();
 }
