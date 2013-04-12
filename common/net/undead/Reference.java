0465d6002dd2d1a6f0369454b0e855c327b51976
Anten <cebarks@gmail.com>
2013-04-12 11:44:52 -0600

    Revert "no message"
    This reverts commit 71cfe55dfb27effe29411a34f3d669f5f5d5d7ea.

--- common/net/undead/Reference.java
+++ common/net/undead/Reference.java
@@ -4,17 +4,13 @@ import cpw.mods.fml.client.FMLClientHandler;
 import net.minecraft.client.Minecraft;
 
 public class Reference {
+	public static final String TextureLocation = "/mods/UndeadMinecraft/textures";
+	public static final String EntityTextureLocation = "/mods/UndeadMinecraft/textures/entity/";
 	public static final String MOD_ID = "UndeadMinecraft";
 	public static final String MOD_NAME = "Undead Minecraft";
-	
 	private static final int versionMajor = 2;
 	private static final int versionMinor = 0;
 	private static final int versionSubminor = 0;
-	
 	public static final String VERSION = versionMajor+"."+versionMinor+"."+versionSubminor;
-	
-	public static final String TextureLocation = "/mods/UndeadMinecraft/textures";
-	public static final String EntityTextureLocation = "/mods/UndeadMinecraft/textures/entity/";
-	
 	public static final Minecraft MC = FMLClientHandler.instance().getClient();
 }
